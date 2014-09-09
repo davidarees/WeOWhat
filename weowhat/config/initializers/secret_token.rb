30134503d9d50ac9548f174875a4d64ac9ccf717
David Rees <davidarees@outlook.com>
2014-09-09 13:39:32 +0100

    initial rails creation

---
+++ weowhat/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Weowhat::Application.config.secret_token = '33dd4d3dcc4ef591f99d7e607ae89d0c64e0fa9d04da2f6d6864578bc122003192885c2840e0f3bd755ec53e1e5ce3d932346febf469bf47f12dae651a3f8863'
