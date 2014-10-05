0de7abd50ce4d5e6b47443fdc0c6c32091b072ed
David Rees <davidarees@outlook.com>
2014-10-05 08:48:07 +0100

    added multi select files and modified initial pages

--- weowhat/app/controllers/static_controller.rb
+++ weowhat/app/controllers/static_controller.rb
@@ -2,6 +2,15 @@ class StaticController < ApplicationController
   def index
   end
 
+  def users_list
+    @users = User.all
+    respond_to do |format|
+      # format.html { render json: @users }
+      format.json { render json: @users }
+    end
+    
+  end
+
   def event_users
     @event = Event.find(params[:id])
     @users = @event.users
