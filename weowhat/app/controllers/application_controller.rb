c4125e794e381242030f661bbed38a64691a597d
David Rees <davidarees@outlook.com>
2014-12-10 11:34:24 +0000

    fixed CSRF token issue with Angular

--- weowhat/app/controllers/application_controller.rb
+++ weowhat/app/controllers/application_controller.rb
@@ -3,6 +3,17 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     authenticated_path
-    
   end
+
+    after_filter  :set_csrf_cookie_for_ng
+
+    def set_csrf_cookie_for_ng
+      cookies['XSRF-TOKEN'] = form_authenticity_token if protect_against_forgery?
+    end
+
+  protected
+
+    def verified_request?
+      super || form_authenticity_token == request.headers['X_XSRF_TOKEN']
+    end 
 end
